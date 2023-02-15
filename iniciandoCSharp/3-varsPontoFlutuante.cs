c17c731ce78ccab156bfba604e7290d53eb1639f
Júlia Luna <85693548+Julialunna@users.noreply.github.com>
2023-02-14 21:33:12 -0300

    usando ponto flutuante

---
+++ iniciandoCSharp/3-varsPontoFlutuante.cs
@@ -0,0 +1,19 @@
+﻿using System;
+class Programa {
+    static void Main(string[] args)
+    {
+
+        Console.WriteLine("Projeto 3 - Variáveis Ponto Flutuante");
+
+
+        double salario;
+        salario = 3000.13;
+        double idade = 7.0 / 5;
+
+        Console.WriteLine(salario);
+        Console.WriteLine(idade);
+        Console.WriteLine("Tecle ENTER para fechar");
+        Console.ReadLine();
+    }
+}
+
