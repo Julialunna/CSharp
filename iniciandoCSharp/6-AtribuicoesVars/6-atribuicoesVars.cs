5f28ff0fee59e9ca3231ba006b2b090baf7b0670
Júlia Luna <85693548+Julialunna@users.noreply.github.com>
2023-02-20 17:03:55 -0300

    aprendendo atribuições de variáveis

---
+++ iniciandoCSharp/6-AtribuicoesVars/6-atribuicoesVars.cs
@@ -0,0 +1,18 @@
+﻿using System;
+class Programa
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Executando projetp 6 - Atribuições de variáveis");
+
+        int idade = 30;
+        int idadeAna = idade;
+
+        Console.WriteLine(idade);
+
+        idade = 25;
+        Console.WriteLine(idadeAna);
+        Console.WriteLine("tecle ENTER para fechar");
+        Console.ReadLine();
+    }
+}
