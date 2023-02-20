6a3515f2645e0e9f9469eaa0b79b294fc90bd888
Júlia Luna <85693548+Julialunna@users.noreply.github.com>
2023-02-20 16:02:30 -0300

    aprendendo caracteres

---
+++ iniciandoCSharp/5-Caracteres.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+class Programa {
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Executando projeto 5 - caracteres e textos");
+
+        char letra = 'a';
+        Console.WriteLine(letra);
+
+        letra = (char)65;
+        Console.WriteLine(letra);
+
+        letra = (char)(65 + 1);
+        Console.WriteLine(letra);
+
+        letra = (char)(86 + 1);
+        Console.WriteLine(letra);
+
+        string primeraFrase = "Alura cursos de tecnologia ";
+        Console.WriteLine(primeraFrase + 2023);
+
+        string vazia = "";
+        Console.WriteLine(vazia);
+
+        string cursos = @"Cursos disponíveis 
+                        - Go 
+                        - C# 
+                        - Python 
+                        - Java";
+        Console.WriteLine(cursos); 
+
+        //não pode ter caracter vazia char vazia = ''; -> X
+
+        Console.WriteLine("tecle ENTER para fechar");
+        Console.ReadLine();
+    }
+}
