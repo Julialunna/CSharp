11ee87b7f90f1112be0ebdb4d5bac61fcfbb18de
Júlia Luna <85693548+Julialunna@users.noreply.github.com>
2023-03-12 18:27:31 -0300

    aprendendo break

---
+++ iniciandoCSharp/13-EncadeandoFor/EncadeandoFor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+class EncadeandoFor{
+    static void Main(string[] args){
+        Console.WriteLine("Executando o projeto 13 - Encadeando For ");
+
+        for(int contadorLinhas=0;contadorLinhas<10;contadorLinhas++){
+            for(int contadorColuna=0;contadorColuna<10;contadorColuna++){
+                Console.Write("*");
+                if(contadorColuna>=contadorLinhas){
+                    break;
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
