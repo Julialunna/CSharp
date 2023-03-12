16ff5b8f3f532ad87b85e43d308fc32669aa3a57
Júlia Luna <85693548+Julialunna@users.noreply.github.com>
2023-03-12 18:32:21 -0300

    fazendo desafio  múltiplos de 3

---
+++ iniciandoCSharp/14-Desafios/1-MultiplosDeTres/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+class Program{
+    static void Main(string[]args){
+        int numero;
+        for(numero=0;numero<=100;numero++){
+            if (numero%3==0){
+                Console.WriteLine(numero);
+            }
+        }
+    }
+}
