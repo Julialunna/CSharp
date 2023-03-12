f014f54626c7a1077a0dd2aeb84363a0df7094b1
Júlia Luna <85693548+Julialunna@users.noreply.github.com>
2023-03-12 18:41:56 -0300

    fazendo desafio tutorial

---
+++ iniciandoCSharp/14-Desafios/2-Fatorial/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+class Program{
+    static void Main(string[] args){
+        int num;
+        int fatorial=1;
+        for(num=1;num<=10;num++){
+            fatorial=1;
+            for(int i=1;i<=num;i++){
+                fatorial=fatorial*i;
+            }
+            Console.WriteLine("Fatorial de "+num+" é "+fatorial);
+        }
+    }
+}
