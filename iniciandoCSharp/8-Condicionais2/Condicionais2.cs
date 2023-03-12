388ed166f7c57bfcd8f895aba5f78319eec8fc9e
Júlia Luna <85693548+Julialunna@users.noreply.github.com>
2023-03-12 14:28:48 -0300

    usando bool

---
+++ iniciandoCSharp/8-Condicionais2/Condicionais2.cs
@@ -0,0 +1,21 @@
+﻿using System;
+class Condicionais2{
+    static void Main(string[] args){
+        Console.WriteLine("Executando o projeto 8 - Condicionais 2");
+
+        int idadeJoao=16;
+        int quantidadePessoas=2;
+
+        bool acompanhado=quantidadePessoas>1;
+        bool grupo=true;
+
+        if(idadeJoao>=18 ||grupo){
+            Console.WriteLine("Pode entrar!");
+        }else {
+            Console.WriteLine("Não pode entrar");
+        }
+
+        Console.WriteLine("Tecle enter para fechar...");
+        Console.ReadLine();
+    }
+}
