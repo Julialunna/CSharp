39cdeb049ed68626e1d63069f39efcdd4be44aad
Júlia Luna <85693548+Julialunna@users.noreply.github.com>
2023-03-13 16:30:37 -0300

    começando a classe

---
+++ byteBank/ContaCorrente.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace byteBank{
+    class ContaCorrente{
+        public int numero_agencia;
+        public string conta;
+        public string titular;
+        public double saldo;
+    }
+
+}
---
+++ byteBank/Program.cs
@@ -0,0 +1,9 @@
+﻿using byteBank;
+
+ContaCorrente contaDoAndre=new ContaCorrente();
+contaDoAndre.titular="André Silva";
+contaDoAndre.numero_agencia=15;
+contaDoAndre.conta="1010-X";
+contaDoAndre.saldo=100;
+
+Console.WriteLine("Saldo da conta o André: "+contaDoAndre.saldo);
